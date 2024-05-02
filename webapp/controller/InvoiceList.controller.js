5e57c6266233ff6e5b09172cb9cd19834a453d33
Amir Salimi <85235736+amirsalimi92@users.noreply.github.com>
2024-05-02 15:58:06 +0200

    02.05_step24

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -3,8 +3,10 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  function (Controller, JSONModel, formatter) {
+  function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
       formatter: formatter,
@@ -13,6 +15,21 @@ sap.ui.define(
           currency: "EURO",
         });
         this.getView().setModel(oViewModel, "view");
+      },
+      onFilterInvoices: function (oEvent) {
+        // Build filter array
+        let aFilter = [];
+        let sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductName", FilterOperator.Contains, sQuery)
+          );
+        }
+
+        // Filtr binding
+        let oList = this.byId("invoiceList");
+        let oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
       },
     });
   }
